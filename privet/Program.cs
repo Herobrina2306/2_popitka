e9724b7985acec532a9755c6d234ad62837f8bc5
AleksandraM <musatova.aleksandra@mail.ru>
2022-08-28 15:04:30 +0300

    Особое приветствие

---
+++ privet/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "яна")
+{
+    Console.WriteLine("Ура, Привет тебе Яночка!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
